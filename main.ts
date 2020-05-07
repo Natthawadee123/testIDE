a2423c9e1e0976f2d0055fbbbc5cea9cb88b3a5d
Natthawadee123 <61381230+Natthawadee123@users.noreply.github.com>
2020-05-07 11:02:01 +0700

    addd log

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 function calculator(num1,num2){
     let sum = num1+num2;
-
+    console.log("+++======+++");
+    console.log("+++======+++");
+    console.log("+++======+++");
+    console.log("+++======+++");
     console.log("+++======+++");
     console.log("summation: ",num1," + ",num2," = ",sum);
     console.log("+++======+++");
