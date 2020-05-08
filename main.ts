60142931887b06a004b2f70631d57f372eff15fe
Natthawadee123 <61381230+Natthawadee123@users.noreply.github.com>
2020-05-08 15:34:17 +0700

    2 function

--- main.ts
+++ main.ts
@@ -4,3 +4,11 @@ function calculator(num1,num2){
     console.log("summation: ",num1," + ",num2," = ",sum);
 
 }
+
+
+function calculator(num1,num2){
+    let sum = num1+num2;
+    let sum01 = num1;
+    console.log("summation: ",num1," + ",num2," = ",sum);
+
+}
